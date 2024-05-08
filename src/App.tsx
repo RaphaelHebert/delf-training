f78271ccd42c1b53c6378cb0f76c14c7fcef0138
raphael-corize <raphael.hebert@corize.co.jp>
2024-05-08 12:22:15 +0200

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import '@/App.css'
+import { Routes, Route } from 'react-router-dom'
 import DiceRoll from './pages/DiceRoll'
+import Login from './pages/Login'
 
 function App() {
-  return <DiceRoll />
+  return (
+    <Routes>
+      {/* TODO: add a loader to the Route */}
+      <Route
+        path='/'
+        element={<DiceRoll />}
+      />
+      <Route
+        path='/login'
+        element={<Login />}
+      />
+    </Routes>
+  )
 }
 export default App
