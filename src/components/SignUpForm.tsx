abfeceabf73772801329ce6405b4189f7147a557
raphael-corize <raphael.hebert@corize.co.jp>
2024-05-26 10:13:01 +0200

    added password requirements to sign up

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -92,8 +92,11 @@ const SignUpForm: React.FC = () => {
       .required('Username is required'),
     password: yup
       .string()
-      .min(8, 'Password must be at least 8 characters')
-      .required('Password is required'),
+      .required('Please Enter your password')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+        'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Character $!@#%&^*'
+      ),
     confirmPassword: yup
       .string()
       .oneOf([yup.ref('password')], 'Passwords must match')
