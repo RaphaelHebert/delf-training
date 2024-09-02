ad2f3059d158dd32af1eecbd3e36e810eca21b82
raphael-corize <raphael.hebert@corize.co.jp>
2024-09-02 19:45:43 +0200

    updated

---
+++ src/data/questionsA1.ts
@@ -0,0 +1,47 @@
+export const a1 = [
+  {
+    title: 'Level 1 debutant',
+    qcm: {
+      question: 'Je _____ Francais.',
+      answers: ['ete', 'suis', 'ai', 'ne suis'],
+      correct: 'suis',
+    },
+    instructions: 'Choisissez la bonne reponse:',
+  },
+  {
+    title: 'Level 1 debutant',
+    qcm: {
+      question: 'Ils _____ manger des glaces.',
+      answers: ['aiment', 'aime', 'aimes', 'aimer'],
+      correct: 'aiment',
+    },
+    instructions: 'Choisissez la bonne reponse:',
+  },
+  {
+    title: 'Level 1 debutant',
+    qcm: {
+      question: '_____-tu cet homme?',
+      answers: ['Connaissez', 'Connetrons', 'Connais', 'Sais'],
+      correct: 'Connais',
+    },
+    instructions: 'Choisissez la bonne reponse:',
+  },
+  {
+    title: 'Level 1 debutant',
+    qcm: {
+      question: 'Vous _____ tres grands.',
+      answers: ['aimez', 'mesurez', 'avez', 'etes'],
+      correct: 'etes',
+    },
+    instructions: 'Choisissez la bonne reponse:',
+  },
+  {
+    title: 'Level 1 debutant',
+    qcm: {
+      question: 'Nous _____ pas les carottes.',
+      answers: ["n'aimons", 'detestons', 'aimons', 'ne savons'],
+      correct: "n'aimons",
+    },
+    instructions: 'Choisissez la bonne reponse:',
+  },
+]
