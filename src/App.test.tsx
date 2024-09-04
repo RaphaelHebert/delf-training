8ca60b61db6a5fc7ba9c97cbfad0dd4077259bce
raphael-corize <raphael.hebert@corize.co.jp>
2024-09-04 20:04:14 +0200

    updated UTs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -21,7 +21,7 @@ describe('Renders main page correctly', () => {
       </BrowserRouter>
     )
 
-    const home = await screen.queryByText('Hiome')
+    const home = await screen.queryByText('Home')
 
     expect(home).toBeInTheDocument()
   })
