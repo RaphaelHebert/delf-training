3dc81bb538a9b4f67afc4d5d54713b1092a009b5
raphael-corize <raphael.hebert@corize.co.jp>
2024-09-05 21:15:14 +0200

    added questions

--- src/data/questionsA1.ts
+++ src/data/questionsA1.ts
@@ -1,47 +1,98 @@
 export const a1 = [
   {
-    title: 'Level 1 debutant',
-    qcm: {
-      question: 'Je _____ Francais.',
-      answers: ['ete', 'suis', 'ai', 'ne suis'],
-      correct: 'suis',
-    },
-    instructions: 'Choisissez la bonne reponse:',
+    question: 'Vous _____ le temps pour une interview ?',
+    answers: ['avez', 'avons', 'vont', 'va'],
+    correct: 'avez',
   },
   {
-    title: 'Level 1 debutant',
-    qcm: {
-      question: 'Ils _____ manger des glaces.',
-      answers: ['aiment', 'aime', 'aimes', 'aimer'],
-      correct: 'aiment',
-    },
-    instructions: 'Choisissez la bonne reponse:',
+    question: 'Vous _____ me tutoyer, vous savez.',
+    answers: ['pouvez', 'peux', 'pouvons', 'peuvent'],
+    correct: 'pouvez',
   },
   {
-    title: 'Level 1 debutant',
-    qcm: {
-      question: '_____-tu cet homme?',
-      answers: ['Connaissez', 'Connetrons', 'Connais', 'Sais'],
-      correct: 'Connais',
-    },
-    instructions: 'Choisissez la bonne reponse:',
+    question: 'Tu _____ bien et présenter à nos auditeurs ?',
+    answers: ['veux', 'veut', 'voulons', 'voulez'],
+    correct: 'veux',
   },
   {
-    title: 'Level 1 debutant',
-    qcm: {
-      question: 'Vous _____ tres grands.',
-      answers: ['aimez', 'mesurez', 'avez', 'etes'],
-      correct: 'etes',
-    },
-    instructions: 'Choisissez la bonne reponse:',
+    question: 'Bien sûr. Voilà, je _____ Louise.',
+    answers: ["m'appelle", 'ai', "s'appelle", 'vous appelez'],
+    correct: "m'appelle",
   },
   {
-    title: 'Level 1 debutant',
-    qcm: {
-      question: 'Nous _____ pas les carottes.',
-      answers: ["n'aimons", 'detestons', 'aimons', 'ne savons'],
-      correct: "n'aimons",
-    },
-    instructions: 'Choisissez la bonne reponse:',
+    question: "J' _____ 18 ans.",
+    answers: ['ont', 'avez', 'avons', 'ai'],
+    correct: 'ai',
+  },
+  {
+    question: 'Je _____ de Montréal et maintenant, j’habite à Paris.',
+    answers: ['vient', 'venons', 'viens', 'venez'],
+    correct: 'viens',
+  },
+  {
+    question: 'Je _____ un appartement avec une étudiante japonaise, Keiko.',
+    answers: ['partager', 'partage', 'partagez', 'partageons'],
+    correct: 'partage',
+  },
+  {
+    question: 'Elle _____ les mêmes cours que moi.',
+    answers: ['suivons', 'suis', 'suivent', 'suit'],
+    correct: 'suit',
+  },
+  {
+    question: "Nous _____ ensemble à l'école.",
+    answers: ['vais', 'allons', 'vont', 'va'],
+    correct: 'allons',
+  },
+  {
+    question: 'Il _____ trente minutes environ.',
+    answers: ['faut', 'fallait', 'faille', 'falloir'],
+    correct: 'faut',
+  },
+  {
+    question: 'Quels _____ tes loisirs ?',
+    answers: ['suis', 'sont', 'êtes', 'sommes'],
+    correct: 'sont',
+  },
+  {
+    question: 'Je _____ des bandes dessinées.',
+    answers: ['fais', 'dessine', 'fait', 'font'],
+    correct: 'fais',
+  },
+  {
+    question: 'Tu _____ bien Paris?',
+    answers: ['connaître', 'connais', 'connaissent', 'connaissons'],
+    correct: 'connais',
+  },
+  {
+    question: 'Pas encore mais je _____ beaucoup pour découvrir la ville.',
+    answers: ['nous promenons', 'se promènent', 'te promènes', 'me promène'],
+    correct: 'me promène',
+  },
+  {
+    question:
+      'En semaine, Keiko et moi, nous _____ beaucoup parce que nous devons étudier.',
+    answers: ['ne sors pas', 'ne sortons pas', 'ne sortent pas', 'sors'],
+    correct: 'ne sors pas',
+  },
+  {
+    question: 'Le week-end, avec nos amis, nous _____ la fête.',
+    answers: ['faites', 'fais', 'faisons', 'faisons'],
+    correct: 'faisons',
+  },
+  {
+    question: 'Je me _____ tous les jours.',
+    answers: ['laver', 'bois', 'mange', 'lave'],
+    correct: 'lave',
+  },
+  {
+    question: 'Nous _____ la radio.',
+    answers: ['entendons', 'regardons', 'écoutons', 'voyons'],
+    correct: 'écoutons',
+  },
+  {
+    question: 'Ils _____ un film ensemble.',
+    answers: ['voient', 'regardent', 'écoutent', 'entendent'],
+    correct: 'regardent',
   },
 ]
