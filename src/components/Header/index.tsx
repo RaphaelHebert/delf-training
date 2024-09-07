842406c395795360efea1b34c8b8e52e90e26f5c
raphael-corize <raphael.hebert@corize.co.jp>
2024-09-07 19:26:36 +0200

    added toggle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,63 @@
-import { Flex, Button } from '@radix-ui/themes'
-import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+import { useState } from 'react'
+import { Flex, Text, Switch } from '@radix-ui/themes'
+// import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 
 import React from 'react'
 
 const Header: React.FC = () => {
+  const [isExamMode, setIsExamMode] = useState(false)
+
+  const toggleExamMode = () => {
+    setIsExamMode((prev) => !prev)
+  }
+
   return (
     <Flex
-      justify='start'
+      px='6'
+      justify='between'
       align='center'
       style={{
         borderBottom: '1px solid lightGrey',
       }} //for dev
       height='5rem'
     >
-      <Button mx='3'>
+      {/* <Button mx='3'>
         <HamburgerMenuIcon />
-      </Button>
-      DELF Training
+      </Button> */}
+      <Text
+        as='label'
+        size='2'
+      >
+        {' '}
+        DELF Training
+      </Text>
+
+      <Text
+        as='label'
+        size='2'
+      >
+        <Flex gap='2'>
+          <Text
+            as='span'
+            color={isExamMode ? 'gray' : 'mint'}
+            style={isExamMode ? {} : { fontWeight: 700 }}
+          >
+            Training mode
+          </Text>
+          <Switch
+            size='1'
+            checked={isExamMode} // Bind the state to the switch
+            onCheckedChange={toggleExamMode}
+          />
+          <Text
+            as='span'
+            color={isExamMode ? 'mint' : 'gray'}
+            style={isExamMode ? { fontWeight: 700 } : {}}
+          >
+            Exam mode
+          </Text>
+        </Flex>
+      </Text>
     </Flex>
   )
 }
