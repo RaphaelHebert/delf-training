20f9ee7405592c3c014514a4f1976711f108068e
raphael-corize <raphael.hebert@corize.co.jp>
2024-09-14 11:22:21 +0200

    feat(Results): added size prop

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -6,6 +6,7 @@ import './styles.css'
 type Props = {
   percent: number
   speed: number // animation time in seconds
+  size: number //the size in px
   goBack?: () => void
 }
 
@@ -18,15 +19,25 @@ const updateAnimationSpeed = (speed: number): void => {
   document.documentElement.style.setProperty('--speed', `${speed}s`)
 }
 
-const Results: React.FC<Props> = ({ percent, goBack, speed = 3000 }) => {
+const updateSize = (size: number): void => {
+  document.documentElement.style.setProperty('--size', `${size}px`)
+}
+
+const Results: React.FC<Props> = ({
+  percent,
+  goBack,
+  speed = 3000,
+  size = 400,
+}) => {
   const [classNameAnimation, setClassNameAnimation] = useState('resultPercent')
   const [percentAnimation, setPercentAnimation] = useState(0)
 
   useEffect(() => {
     updatePercentDisplay(percent)
     updateAnimationSpeed(speed)
+    updateSize(size)
     setClassNameAnimation('resultPercent resultPercentAnimation')
-  }, [percent, speed])
+  }, [percent, speed, size])
 
   useEffect(() => {
     if (percentAnimation !== percent) {
@@ -45,6 +56,7 @@ const Results: React.FC<Props> = ({ percent, goBack, speed = 3000 }) => {
       justify='start'
       align='stretch'
       width='100%'
+      className='setSize'
     >
       <div className='resultPercentBackground'>
         <div
