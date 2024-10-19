7f5b14f8d9d1259ebf007322ecbf4fdb5ea7e76a
raphael-corize <raphael.hebert@corize.co.jp>
2024-10-19 18:06:16 +0200

    fix(router): remmoved base

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import '@radix-ui/themes/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 //import { QueryClient, QueryClientProvider } from 'react-query'
 import { Theme } from '@radix-ui/themes'
 import { ModeAndLevelProvider } from '@/contexts/modeAndLevel'
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         mode: 'training',
       }}
     >
-      <BrowserRouter basename='/delf-training'>
+      <HashRouter basename='/'>
         {/* <QueryClientProvider client={queryClient}> */}
         <Theme
           accentColor={COLOR_CORRECT}
@@ -33,7 +33,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <App />
         </Theme>
         {/* </QueryClientProvider> */}
-      </BrowserRouter>
+      </HashRouter>
     </ModeAndLevelProvider>
   </React.StrictMode>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/delf-training/',
   resolve: {
     alias: [
       {
