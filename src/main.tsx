8b337aa71ef7670e9d7df55878504eb2755dd970
Raphael Hebert <41419062+RaphaelHebert@users.noreply.github.com>
2024-10-27 00:43:04 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         mode: 'training',
       }}
     >
-      <BrowserRouter basename='delf-training/'>
+      <BrowserRouter basename='/'>
         {/* <QueryClientProvider client={queryClient}> */}
         <Theme
           accentColor={COLOR_CORRECT}
